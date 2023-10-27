d16462009b0076ea7b0d143d2c3fdfe1e9875cf9
niik <fk724s@mail.ru>
2023-10-27 20:57:58 +0300

    Initial commit

---
+++ src/main/java/com/example/weblab3/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.example.weblab3;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/index.xhtml").forward(request, response);
+    }
+
+}
