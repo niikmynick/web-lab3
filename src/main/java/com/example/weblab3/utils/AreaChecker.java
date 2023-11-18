6b4f85369683ed16d2639da037677ba9e6010691
niik <fk724s@mail.ru>
2023-11-18 16:30:10 +0300

    changed figure

--- src/main/java/com/example/weblab3/utils/AreaChecker.java
+++ src/main/java/com/example/weblab3/utils/AreaChecker.java
@@ -4,13 +4,13 @@ public final class AreaChecker {
     public static boolean isHit(double x, double y, double r) {
         if (x >= 0) {
             if (y >= 0) {
-                return x + y <= r;
+                return Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow((r/2), 2);
             } else {
-                return (x <= (r / 2)) && (y >= -r);
+                return (x <= r) && (y >= -r);
             }
         } else {
             if (y >= 0) {
-                return Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow((r/2), 2);
+                return (-x + y) <= (r / 2);
             } else {
                 return false;
             }
