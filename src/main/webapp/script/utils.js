1aa04d4eb29b36a299de12046e45e2f8302b2c5c
niik <fk724s@mail.ru>
2023-11-19 14:20:28 +0300

    spaces

--- src/main/webapp/script/utils.js
+++ src/main/webapp/script/utils.js
@@ -34,7 +34,7 @@ export function getXValues() {
 export function checkStatus(x, y, r) {
     if (x >= 0) {
         if (y >= 0) {
-            return Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow((r/2), 2);
+            return Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow((r / 2), 2);
         } else {
             return (x <= r) && (y >= -r);
         }
